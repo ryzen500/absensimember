493e8297c3454409536461aa2e1c8a8c5b16fc58
ryzen500 <72720477+ryzen500@users.noreply.github.com>
2021-08-15 20:01:14 +0700

    Pembuatan Grafik Absensi sudah muncul
    Noted :  belum  dikonfigurasikan dengan database

--- admin2.php
+++ admin2.php
@@ -34,6 +34,10 @@
     <!-- Main CSS-->
     <link href="css/theme.css" rel="stylesheet" media="all">
 
+    <!-- Main the Call Script -->
+    <link rel="stylesheet" href="vendor/chartjs/Chart.min.css">
+    <script type="text/javascript" src="vendor/chartjs/Chart.bundle.min.js"></script>
+
 </head>
 
 <body class="animsition">
@@ -223,7 +227,8 @@
                         <div class="row">
                             <center><img src="img/logokaryawan.png" width="500" class="img img-responsive" height="500"></center><br>
                             <strong class="col-md-12" style="margin-top: 5rem;">Selamat datang admin SIKaryawan</strong>
-
+                            <!-- Grafik -->
+                            <canvas id="myChart"></canvas>
                                     </div>
                                 </div>
                             </div>
@@ -243,6 +248,51 @@
         </div>
 
     </div>
+    <script>
+//deklarasi chartjs untuk membuat grafik 2d di id mychart 
+var ctx = document.getElementById('myChart').getContext('2d');
+
+var myChart = new Chart(ctx, {
+ //chart akan ditampilkan sebagai bar chart
+    type: 'bar',
+    data: {
+     //membuat label chart
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            //isi chart
+            data: [12, 19, 3, 5, 2, 3],
+            //membuat warna pada bar chart
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+</script>
 
     <!-- Jquery JS-->
     <script src="vendor/jquery-3.2.1.min.js"></script>
